357b97256d7d77f2cf675ee2d666c45e94c23f04
khanmuha <muhammad.khan@gmx.co.uk>
2018-12-04 23:23:17 +0000

    added contents for deleteAccount test

--- QAdemo/src/test/java/muhammadkhan/QAdemo/QAdemoApplicationTests.java
+++ QAdemo/src/test/java/muhammadkhan/QAdemo/QAdemoApplicationTests.java
@@ -1,27 +1,43 @@
 package muhammadkhan.QAdemo;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class QAdemoApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+
+
+	
 	@Test
-	public void retreiveAllAccount() {
+	public void retreiveAllAccount() throws Exception {
+		
+		this.mvc.perform(get("/rest/account/json")).andExpect(status().isOk());		
 		
 	}
 	
 	@Test
 	public void addAccount() {
-		
+	
 	}
 	
 	@Test
-	public void deleteAccount() {
-		
+	public void deleteAccount() throws Exception {
+		this.mvc.perform(get("/rest/account/json/1")).andExpect(status().isOk());	
 	}
 	
 }
