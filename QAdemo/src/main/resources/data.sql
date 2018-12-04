01b06feafd0d90e16cd7a4ef738724dd4f790b52
khanmuha <muhammad.khan@gmx.co.uk>
2018-12-04 23:30:35 +0000

    changed sql

--- QAdemo/src/main/resources/data.sql
+++ QAdemo/src/main/resources/data.sql
@@ -1,4 +1,4 @@
 delete from account;
-insert into account (first_name,last_name,account_number) values ('John','Doe','1234');
+insert into account (id,first_name,last_name,account_number) values (1,'John','Doe','1234');
 insert into account (first_name,last_name,account_number) values ('John','Doe','1235');
 insert into account (first_name,last_name,account_number) values ('Jim','Taylor','1236');
