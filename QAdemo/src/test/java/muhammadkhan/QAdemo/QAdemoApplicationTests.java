a2ea0c0459d3436d2c2ef98d93077238705a7446
khanmuha <muhammad.khan@gmx.co.uk>
2018-12-04 23:46:00 +0000

    added code for addAccouint test and modified retreeival and delete test

--- QAdemo/src/test/java/muhammadkhan/QAdemo/QAdemoApplicationTests.java
+++ QAdemo/src/test/java/muhammadkhan/QAdemo/QAdemoApplicationTests.java
@@ -1,16 +1,23 @@
 package muhammadkhan.QAdemo;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import muhammadkhan.QAdemo.model.Account;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -25,19 +32,33 @@ public class QAdemoApplicationTests {
 	@Test
 	public void retreiveAllAccount() throws Exception {
 		
-		this.mvc.perform(get("/rest/account/json")).andExpect(status().isOk());		
+		this.mvc.perform(get("/rest/account/json")).andExpect(status().isOk())
+		.andExpect(content().contentType(MediaType.APPLICATION_JSON));		
 		
 	}
 	
 	@Test
-	public void addAccount() {
-	
+	public void addAccount() throws Exception {
+		ObjectMapper objectMapper = new ObjectMapper();
+		Account account = new Account();
+		account.setFirstName("Steven");
+		account.setLastName("Doe");
+		account.setAccountNumber("123456");
+        byte[] requestJson =  objectMapper.writeValueAsBytes(account);
+		
+		this.mvc.perform(post("/rest/account/json")
+		.contentType(MediaType.APPLICATION_JSON)
+		.content(requestJson))        
+        .andExpect(status().isOk());             
+        
+		
 	}
 	
 	@Test
 	public void deleteAccount() throws Exception {
 		
-		this.mvc.perform(get("/rest/account/json/1")).andExpect(status().isOk());
+		this.mvc.perform(delete("/rest/account/json/1")).andExpect(status().isOk())
+		.andExpect(content().contentType(MediaType.APPLICATION_JSON));
 		
 	}
 }
