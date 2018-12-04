ae83d7e45e840d8194ef7f591960d8c07dceb819
khanmuha <muhammad.khan@gmx.co.uk>
2018-12-04 21:37:22 +0000

    base version

---
+++ QAdemo/src/main/java/muhammadkhan/QAdemo/QAdemoApplication.java
@@ -0,0 +1,12 @@
+package muhammadkhan.QAdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QAdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QAdemoApplication.class, args);
+	}
+}
---
+++ QAdemo/src/test/java/muhammadkhan/QAdemo/QAdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package muhammadkhan.QAdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QAdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
